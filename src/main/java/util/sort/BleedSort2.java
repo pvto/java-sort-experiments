9e011244f8f81d0f242b45dcd0623139847b5ad0
Paavo Toivanen <paavo.v.toivanen@gmail.com>
2015-09-01 17:24:53 +0300

    BleedSort2 + initial testing

---
+++ src/main/java/util/sort/BleedSort2.java
@@ -0,0 +1,255 @@
+package util.sort;
+
+import static java.lang.Integer.sum;
+import java.util.Arrays;
+import util.Int;
+
+/**
+ * @author pvto https://github.com/pvto
+ */
+public class BleedSort2 {
+    
+    public static void bleedSort(int[] a)
+    {
+//        if (a.length < 100_000)
+//        {
+//            java.util.Arrays.sort(a);
+//            return;
+//        }
+        
+        int 
+                sampleSize = 60,
+                sign = 1,
+                signChanges = 0
+                ;
+        int[] 
+                quantiles = new int[5],
+                sample = new int[sampleSize]
+                ;
+        for(int i = 0; i < sampleSize; i++)
+        {
+            int 
+                    ind = (int) (Math.random() * (a.length - 1)),
+                    x1 = a[ind],
+                    x2 = a[ind + 1],
+                    signChange = Int.sign(x2 - x1)
+                    ;
+            if (signChange != sign)
+                signChanges++;
+            sign = signChange;
+            sample[i] = a[ind];
+        }
+        Arrays.sort(sample);
+        quantiles[0] = sample[0];
+        quantiles[1] = sample[sample.length >>> 2];
+        quantiles[2] = sample[sample.length >>> 1];
+        quantiles[3] = sample[(sample.length >>> 1) + (sample.length >>> 2)];
+        quantiles[4] = sample[sample.length - 1];
+        
+        double sameAvg = 0;
+        for(int x = 0; x < 20; x++)
+        {
+            int 
+                    ind = (int) (Math.random() * (a.length - 101)),
+                    sameCount = 0
+                    ;
+            for (int i = 1; i < 101; i++)
+                if (a[ind + i] == a[ind])
+                    sameCount++;
+            sameAvg += sameCount;
+        }
+        sameAvg = sameAvg / 20.0 
+                + 1e-6              // add a small expectation
+                ;
+        sameAvg = sameAvg * a.length / 100.0;
+
+        if (sameAvg > 8.0)
+        {
+            QuickSort.quickSort(a, 0, a.length - 1, quantiles[2], QuickSort.maxIsThreshold(a.length));
+            return;
+        }
+
+        int 
+                sameness = fillLSDs((int)sameAvg),
+                tmpSize = (int)((a.length * 4))
+                ;
+
+        bleedSort(a, Int.fill(tmpSize, Integer.MIN_VALUE), quantiles, sameness, 0);
+    }
+    
+    public static int fillLSDs(int x)
+    {
+        int y = 0x01;
+        while(y < x)
+        {
+            x |= y;  y <<= 1;
+        }
+        return x;
+    }
+    
+    
+    public static void bleedSort(int[] a, int[] tmp, int[] quantiles, int sameness, int bleeding)
+    {
+        if (bleeding > 2)
+        {
+            Arrays.sort(a);
+            return;
+        }
+
+        int[] baselines = new int[] {
+                (tmp.length >>> 3), // space for first quartile items; leave some left padding
+                (tmp.length >>> 3) + (tmp.length * 3 >>> 4),  // space for second quartile items
+                (tmp.length >>> 3) + (tmp.length * 6 >>> 4),  // space for third quartile items
+                (tmp.length >>> 3) + (tmp.length * 9 >>> 4),  // space for fourth quartile items; leave some right padding
+                (tmp.length * 7 >>> 3)
+        };
+        double[] mult = {
+                    (baselines[1] - baselines[0]) / (double) (quantiles[1] - quantiles[0]),
+                    (baselines[2] - baselines[1]) / (double) (quantiles[2] - quantiles[1]),
+                    (baselines[3] - baselines[2]) / (double) (quantiles[3] - quantiles[2]),
+                    (baselines[4] - baselines[3]) / (double) (quantiles[4] - quantiles[3])
+        };
+//        System.out.println("quantiles: " + Arrays.toString(quantiles));
+//        System.out.println("baselines: " + Arrays.toString(baselines));
+//        System.out.println("multipliers: " + Arrays.toString(mult));
+        int     // these help in reacting to bad estimates of the distribution
+                minHelper = 0, 
+                minHelpCount = 0,
+                maxHelper = tmp.length - 1, 
+                maxHelpCount = 0,
+                bleedCount = 0,
+                bleedPatch = 0
+                ;
+        for(int i = 0; i < a.length; i++)
+        {
+            int 
+                    x = a[i],
+                    place;
+            if (x < quantiles[2])
+            {
+                if (x < quantiles[1])
+                {
+                    place = baselines[0]
+                            + (int) ((x - quantiles[0]) * mult[0])
+                            ;
+                }
+                else 
+                {
+                    place = baselines[1]
+                            + (int) ((x - quantiles[1]) * mult[1])
+                            ;
+                }
+                if (place < 0)
+                {
+                    place = minHelper;
+                    while (tmp[place] != Integer.MIN_VALUE)
+                        place++;
+                    minHelper = place;
+                    minHelper++;
+                    if (minHelpCount++ > 100 && i < a.length >>> 1)
+                    {   // problem in sample distribution (minimum was estimated too big)
+                        // forsake our trial and run again!
+                        // first adjust minimum
+                        for(int j = 0; j < baselines[0]; j++)
+                        {
+                            if (tmp[j] != Integer.MIN_VALUE && tmp[j] < quantiles[0])
+                                quantiles[0] = tmp[j];
+                        }
+                        // then expand tmp-array and try again
+                        tmp = newTmpArr(tmp, a);
+                        bleedSort(a, tmp, quantiles, sameness, ++bleeding);
+                        return;
+                    }
+                    tmp[place] = x;
+                    continue;
+                }
+            }
+            else if (x < quantiles[3])
+            {
+                place = baselines[2]
+                        + (int) ((x - quantiles[2]) * mult[2])
+                        ;
+            }
+            else
+            {
+                place = baselines[3]
+                        + (int) ((x - quantiles[3]) * mult[3])
+                        ;
+                if (place >= tmp.length)
+                {
+                    place = maxHelper;
+                    while (tmp[place] != Integer.MIN_VALUE)
+                        place--;
+                    maxHelper = place;
+                    if (maxHelpCount++ > 100 && i < a.length >>> 1)
+                    {   // problem in sample distribution (maximum was estimated too small)
+                        // forsake our trial and run again!
+                        // first adjust minimum and maximum
+                        for(int j = 0; j < baselines[0]; j++)
+                        {
+                            if (tmp[j] != Integer.MIN_VALUE && tmp[j] < quantiles[0])
+                                quantiles[0] = tmp[j];
+                        }
+                        for(int j = baselines[4]; j < tmp.length; j++)
+                        {
+                            if (tmp[j] > quantiles[4])
+                                quantiles[4] = tmp[j];
+                        }
+                        // then expand tmp-array and try again
+                        tmp = newTmpArr(tmp, a);
+                        bleedSort(a, tmp, quantiles, sameness, ++bleeding);
+                        return;
+                    }
+                    tmp[place] = x;
+                    continue;
+                }
+            }
+
+            // should we "bleed" towards right?
+            while(tmp[place] != Integer.MIN_VALUE)
+            {
+                place++;
+                if (place >= tmp.length || 
+                        (place & sameness) == 0 && bleedCount++ > a.length << 1)
+                {
+                    // first adjust maximum
+                    for(int j = baselines[4]; j < tmp.length; j++)
+                    {
+                        if (tmp[j] > quantiles[4])
+                            quantiles[4] = tmp[j];
+                    }
+                    // then expand tmp-array and try again
+                    tmp = newTmpArr(tmp, a);
+                    bleedSort(a, tmp, quantiles, sameness, ++bleeding);
+                    return;
+                };
+            }
+            
+            tmp[place] = x;
+        }
+//        for(int i = 0; i < tmp.length; i++)
+//        {
+//            if (tmp[i] == Integer.MIN_VALUE)
+//                System.out.print("#");
+//            else
+//                System.out.print(tmp[i] + " ");
+//        }
+//        System.out.println("");
+        for(int i = 0, j = 0; i < a.length; i++)
+        {
+            while(tmp[j] == Integer.MIN_VALUE)
+                j++;
+            a[i] = tmp[j];
+            j++;
+        }
+        Arrays.sort(a);
+    }
+    
+    private static int[] newTmpArr(int[] tmp, int[] a)
+    {
+        if (tmp.length / (double)a.length < 16)
+            tmp = Int.fill((int) ((long)tmp.length * 3L >>> 1), Integer.MIN_VALUE);
+        return tmp;
+    }
+    
+}
---
+++ src/test/java/util/sort/BleedSort2Test.java
@@ -0,0 +1,317 @@
+
+package util.sort;
+
+import java.util.Arrays;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+public class BleedSort2Test {
+
+    @Ignore
+    @Test
+    public void testBS1e7()
+    {
+        int[] orig = new int[(int)1e7];
+        int[] t;
+        
+        System.out.println("priming...");
+        for(int x = 0; x < 3; x++)
+        {
+            for(int i = 0; i < orig.length; i++)
+                orig[i] = (int) (Math.random() * orig.length);
+            t = Arrays.copyOf(orig, orig.length);
+            BleedSort2.bleedSort(t);
+            t = Arrays.copyOf(orig, orig.length);
+            java.util.Arrays.sort(t);
+        }
+        
+        System.out.println("benchmark...");
+        long bs = 0, as = 0;
+        int trials = 20;
+        for(int x = 0; x < trials; x++)
+        {
+
+            orig = new int[(int)1e7];
+            for(int i = 0; i < orig.length; i++)
+                orig[i] = (int) (Math.random() * orig.length);
+
+            t = Arrays.copyOf(orig, orig.length);
+            long start = System.currentTimeMillis();
+            BleedSort2.bleedSort(t);
+            long elapsed = System.currentTimeMillis() - start;
+            bs += elapsed;
+            System.out.println("bleedsort " + elapsed);
+            for(int i = 1; i < t.length; i++)
+                assertTrue(i + ": " + t[i-1] + ">" + t[i], t[i -1] <= t[i]);
+
+
+            t = Arrays.copyOf(orig, orig.length);
+            start = System.currentTimeMillis();
+            java.util.Arrays.sort(t);
+            elapsed = System.currentTimeMillis() - start;
+            System.out.println("Arrays.sort " + elapsed);
+            as += elapsed;
+        }
+        System.out.println("bleedsort[avg]: " + (bs / (double)trials));
+        System.out.println("Arrays.sort[avg]: " + (as / (double)trials));
+//bleedsort[avg]: 626.65
+//Arrays.sort[avg]: 848.45
+    }
+    
+
+    private void similarize(int[] a , double similarityFactor)
+    {
+        for(int i = 0; i < a.length * similarityFactor; i++)
+        {
+            int 
+                    x = (int) (Math.random() * a.length),
+                    y = (int) (Math.random() * a.length)
+                    ;
+            a[x] = a[y];
+        }
+    }
+    private void mix(int[] a, double mixFactor)
+    {
+        for(int i = 0; i < a.length * mixFactor; i++)
+        {
+            int 
+                    x = (int) (Math.random() * a.length),
+                    y = (int) (Math.random() * a.length)
+                    ;
+            int tmp = a[x];
+            a[x] = a[y];
+            a[y] = tmp;
+        }
+    }
+    
+    private void fillRandom(int[] a, double similarityFactor)
+    {
+        for(int i = 0; i < a.length; i++)
+            a[i] = (int) (Math.random() * a.length);
+        similarize(a, similarityFactor);
+    }
+    private void fillDecr(int[] a, double mixFactor, double similarityFactor)
+    {
+        for(int i = 0; i < a.length; i++)
+            a[i] = a.length - i;
+        mix(a, mixFactor);
+        similarize(a, similarityFactor);
+    }
+    private void fillIncr(int[] a, double mixFactor, double similarityFactor)
+    {
+        for(int i = 0; i < a.length; i++)
+            a[i] = a.length - i;
+        mix(a, mixFactor);
+        similarize(a, similarityFactor);
+    }
+    
+    @Test
+    public void testBS1e6()
+    {
+        double similarityFactor = 0.1;
+        double mixFactor = 1.0;
+        boolean 
+                random = false,
+                decreasing = false
+                ;
+        int trials = 20, innerTrials = 1;
+        int[] orig = new int[(int)2e7];
+        int[] t;
+        
+
+        System.out.println("priming...");
+        for(int x = 0; x < 3; x++)
+        {
+            if (random) fillRandom(orig, similarityFactor);
+            else if (decreasing) fillDecr(orig, mixFactor, similarityFactor);
+            else if (!decreasing) fillIncr(orig, mixFactor, similarityFactor);
+            
+            t = Arrays.copyOf(orig, orig.length);
+            BleedSort2.bleedSort(t);
+            t = Arrays.copyOf(orig, orig.length);
+            java.util.Arrays.sort(t);
+        }
+        
+        System.out.println("benchmark...");
+        long bs = 0, as = 0;
+        long start, elapsed;
+        for(int x = 0; x < trials; x++)
+        {
+
+            orig = new int[orig.length];
+            if (random) fillRandom(orig, similarityFactor);
+            else if (decreasing) fillDecr(orig, mixFactor, similarityFactor);
+            else if (!decreasing) fillIncr(orig, mixFactor, similarityFactor);
+
+
+            elapsed = 0;
+            for(int j = 0; j < innerTrials; j++)
+            {
+                start = System.currentTimeMillis();
+                t = Arrays.copyOf(orig, orig.length);
+                BleedSort2.bleedSort(t);
+                elapsed += System.currentTimeMillis() - start;
+            }
+            bs += elapsed;
+            System.out.println("bleedsort " + (elapsed / (double)innerTrials));
+            
+            elapsed = 0;
+            for(int j = 0; j < innerTrials; j++)
+            {
+                start = System.currentTimeMillis();
+                t = Arrays.copyOf(orig, orig.length);
+                java.util.Arrays.sort(t);
+                elapsed += System.currentTimeMillis() - start;
+            }
+            as += elapsed;
+            System.out.println("Arrays.sort " + (elapsed / (double)innerTrials));
+        }
+        double bss, ass;
+        System.out.println("bleedsort[avg]: " + (bss=(bs / (double)trials / (double)innerTrials)));
+        System.out.println("Arrays.sort[avg]: " + (ass=(as / (double)trials / (double)innerTrials)));
+        System.out.println("Factor [for "+(random?"rnd":(decreasing?"decr":"incr"))+" "+orig.length+", simil. "+similarityFactor+", mix. "+mixFactor+"]: " + bss / ass);
+//bleedsort[avg]: 0.3922
+//Arrays.sort[avg]: 0.4777
+//Factor [for 10000, simil. 0]: 0.8210173749214988
+
+//bleedsort[avg]: 4.255
+//Arrays.sort[avg]: 6.0729999999999995
+//Factor [for 100000, simil. 0]: 0.7006421867281409
+
+//bleedsort[avg]: 59.379999999999995
+//Arrays.sort[avg]: 76.09
+//Factor [for 1000000, simil. 0]: 0.78
+//bleedsort[avg]: 62.06
+//Arrays.sort[avg]: 76.5
+//Factor [for 1000000, simil. 0.1]: 0.8112418300653595
+//bleedsort[avg]: 65.66
+//Arrays.sort[avg]: 73.41
+//Factor [for 1000000, simil. 0.2]: 0.8944285519683967
+//bleedsort[avg]: 67.22
+//Arrays.sort[avg]: 71.6
+//Factor [for 1000000, simil. 0.3]: 0.9388268156424582
+//bleedsort[avg]: 69.14
+//Arrays.sort[avg]: 71.22999999999999
+//Factor [for 1000000, simil. 0.4]: 0.9706584304366139
+//bleedsort[avg]: 71.15
+//Arrays.sort[avg]: 76.1
+//Factor [for 1000000, simil. 0.5]: 0.9349540078843628
+//bleedsort[avg]: 74.08
+//Arrays.sort[avg]: 71.25
+//Factor [for 1000000, simil. 0.6]: 1.039719298245614
+//bleedsort[avg]: 74.7
+//Arrays.sort[avg]: 77.05
+//Factor [for 1000000, simil. 0.7]: 0.9695003244646334
+//bleedsort[avg]: 74.66
+//Arrays.sort[avg]: 77.49
+//!!! Factor [for 1000000, simil. 0.7]: 0.9634791586011099
+//bleedsort[avg]: 75.59
+//Arrays.sort[avg]: 71.97999999999999
+//Factor [for 1000000, simil. 0.8]: 1.0501528202278412
+//bleedsort[avg]: 77.69
+//Arrays.sort[avg]: 70.64
+//Factor [for 1000000, simil. 0.9]: 1.0998018120045299
+//bleedsort[avg]: 85.33
+//Arrays.sort[avg]: 73.83
+//Factor [for 1000000, simil. 1.0]: 1.1557632398753894
+
+//bleedsort[avg]: 12.440000000000001
+//Arrays.sort[avg]: 2.2800000000000002
+//Factor [for incr 1000000, simil. 0.0, mix. 0.0]: 5.456140350877193
+//bleedsort[avg]: 11.709999999999999
+//Arrays.sort[avg]: 10.879999999999999
+//Factor [for incr 1000000, simil. 0.0, mix. 3.051758E-6]: 1.0762867647058825
+//bleedsort[avg]: 18.330000000000002
+//Arrays.sort[avg]: 41.239999999999995
+//Factor [for incr 1000000, simil. 0.0, mix. 0.1]: 0.4444713870029099
+//bleedsort[avg]: 24.95
+//Arrays.sort[avg]: 70.38
+//Factor [for incr 1000000, simil. 0.0, mix. 1.0]: 0.3545041204887752
+
+//bleedsort[avg]: 43.95
+//Arrays.sort[avg]: 73.25
+//Factor [for incr 1000000, simil. 0.1, mix. 1.0]: 0.6000000000000001
+//bleedsort[avg]: 58.95
+//Arrays.sort[avg]: 77.1
+//Factor [for incr 1000000, simil. 0.5, mix. 1.0]: 0.764591439688716
+//bleedsort[avg]: 71.6
+//Arrays.sort[avg]: 78.6
+//Factor [for incr 1000000, simil. 1.0, mix. 1.0]: 0.910941475826972
+
+//bleedsort[avg]: 14.690000000000001
+//Arrays.sort[avg]: 3.1399999999999997
+//Factor [for decr 1000000, simil. 0.0, mix. 0.0]: 4.678343949044587
+//bleedsort[avg]: 11.86
+//Arrays.sort[avg]: 10.690000000000001
+//Factor [for decr 1000000, simil. 0.0, mix. 3.051758E-6]: 1.109448082319925
+//bleedsort[avg]: 12.88
+//Arrays.sort[avg]: 11.5
+//Factor [for decr 1000000, simil. 0.0, mix. 6.103516E-6]: 1.12
+//bleedsort[avg]: 12.03
+//Arrays.sort[avg]: 14.63
+//Factor [for decr 1000000, simil. 0.0, mix. 1.220703E-5]: 0.822282980177717
+//bleedsort[avg]: 12.14
+//Arrays.sort[avg]: 16.32
+//Factor [for decr 1000000, simil. 0.0, mix. 2.441406E-5]: 0.7438725490196079
+//bleedsort[avg]: 12.27
+//Arrays.sort[avg]: 15.319999999999999
+//Factor [for decr 1000000, simil. 0.0, mix. 4.882813E-5]: 0.8009138381201045
+//bleedsort[avg]: 12.51
+//Arrays.sort[avg]: 16.580000000000002
+//Factor [for decr 1000000, simil. 0.0, mix. 9.765625E-5]: 0.7545235223160434
+//bleedsort[avg]: 12.309999999999999
+//Arrays.sort[avg]: 16.97
+//Factor [for decr 1000000, simil. 0.0, mix. 1.953125E-4]: 0.7253977607542722
+//bleedsort[avg]: 12.55
+//Arrays.sort[avg]: 18.919999999999998
+//Factor [for decr 1000000, simil. 0.0, mix. 3.90625E-4]: 0.6633192389006344
+//bleedsort[avg]: 13.85
+//Arrays.sort[avg]: 18.2
+//Factor [for decr 1000000, simil. 0.0, mix. 7.8125E-4]: 0.760989010989011
+//bleedsort[avg]: 12.64
+//Arrays.sort[avg]: 19.47
+//Factor [for decr 1000000, simil. 0.0, mix. 0.0015625]: 0.6492039034411916
+//bleedsort[avg]: 12.709999999999999
+//Arrays.sort[avg]: 21.11
+//Factor [for decr 1000000, simil. 0.0, mix. 0.003125]: 0.6020843202273803
+//bleedsort[avg]: 13.25
+//Arrays.sort[avg]: 21.7
+//Factor [for decr 1000000, simil. 0.0, mix. 0.00625]: 0.6105990783410138
+//bleedsort[avg]: 13.85
+//Arrays.sort[avg]: 23.27
+//Factor [for decr 1000000, simil. 0.0, mix. 0.0125]: 0.5951869359690589
+//bleedsort[avg]: 14.77
+//Arrays.sort[avg]: 29.380000000000003
+//Factor [for decr 1000000, simil. 0.0, mix. 0.025]: 0.5027229407760381
+//bleedsort[avg]: 17.009999999999998
+//Arrays.sort[avg]: 32.57
+//Factor [for decr 1000000, simil. 0.0, mix. 0.05]: 0.5222597482345717
+//bleedsort[avg]: 18.259999999999998
+//Arrays.sort[avg]: 40.660000000000004
+//Factor [for decr 1000000, simil. 0.0, mix. 0.1]: 0.4490900147565174
+//bleedsort[avg]: 22.59
+//Arrays.sort[avg]: 62.33
+//Factor [for decr 1000000, simil. 0.0, mix. 0.3]: 0.3624257981710252
+//bleedsort[avg]: 23.16
+//Arrays.sort[avg]: 66.09
+//Factor [for decr 1000000, simil. 0.0, mix. 0.5]: 0.3504312301407172
+//bleedsort[avg]: 24.79
+//Arrays.sort[avg]: 71.72
+//Factor [for decr 1000000, simil. 0.0, mix. 1.0]: 0.34564974902398216
+
+//######## 2e8
+//bleedsort[avg]: 1301.1
+//Arrays.sort[avg]: 1879.5
+//Factor [for 20000000, simil. 0]: 0.6922585794094174
+//bleedsort[avg]: 1631.95
+//Arrays.sort[avg]: 1772.35
+//Factor [for 20000000, simil. 0.5]: 0.9207831410274495
+        
+//bleedsort[avg]: 981.35
+//Arrays.sort[avg]: 1869.35
+//Factor [for incr 20000000, simil. 0.1, mix. 1.0]: 0.5249685719635168
+    }
+ 
+
+}
