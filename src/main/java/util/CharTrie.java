4505173bb776d0d8cc4a36dd3342f0dbffb8f954
Paavo Toivanen <paavo.v.toivanen@gmail.com>
2016-04-24 21:32:16 +0300

    add ordered char trie

--- src/main/java/util/CharTrie.java
+++ src/main/java/util/CharTrie.java
@@ -1,21 +1,34 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /** A char trie. Not thread-safe.
  *
  * @author Paavo Toivanen https://github.com/pvto
  */
-public abstract class CharTrie {
+public abstract class CharTrie implements Comparable<CharTrie> {
 
     public abstract boolean match(char[] chars, int offset);
     public abstract boolean match(char c);
     public abstract void put(char[] chars, int offset);
+    public abstract CharTrie getChild(char c);
 
+    public final char key;
     private int size = 0;
     protected boolean terminal = false;
     
+        
+    public CharTrie()
+    {
+        this.key = (char)0;
+    }
+    public CharTrie(char key)
+    {
+        this.key = key;
+    }
+
     public boolean match(char[] chars)
     {
         return match(chars, 0);
@@ -38,8 +51,67 @@ public abstract class CharTrie {
         return root;
     }
     
+    public static CharTrie newOrderedTrie()
+    {
+        CharTrie root = new OrderedCharTrieRoot();
+        return root;
+    }
+    
+    @Override
+    public int compareTo(CharTrie x)
+    {
+        if (x.key > this.key) return -1;
+        else if (x.key < this.key) return 1;
+        return 0;
+    }
+    
+    public static class DummyCharTrie extends CharTrie {
+        
+        public DummyCharTrie(char key)
+        {
+            super(key);
+        }
+
+        @Override
+        public boolean match(char[] chars, int offset) {
+            throw new UnsupportedOperationException("Dummy.");
+        }
+
+        @Override
+        public boolean match(char c) {
+            throw new UnsupportedOperationException("Dummy.");
+        }
+
+        @Override
+        public void put(char[] chars, int offset) {
+            throw new UnsupportedOperationException("Dummy.");
+        }
+
+        @Override
+        public CharTrie getChild(char c) {
+            throw new UnsupportedOperationException("Dummy.");
+        }
+    }
+    
     public static class CharTrieRoot extends CharTrieLevel0 {
 
+        @Override
+        public boolean match(char[] chars, int offset)
+        {
+            if (offset == chars.length && terminal)
+                return true;
+            char c = chars[offset];
+            CharTrie child = children[c];
+            if (child == null)
+                return false;
+            return child.match(chars, offset + 1);
+        }
+        
+    }
+    
+    public static class OrderedCharTrieRoot extends OrderedCharTrieLevel0 {
+
+        @Override
         public boolean match(char[] chars, int offset)
         {
             if (offset == chars.length && terminal)
@@ -55,23 +127,25 @@ public abstract class CharTrie {
     
     public static class CharTrieLevel0 extends CharTrie {
         
-        public final char key;
         public CharTrie[] children = new CharTrie[0xFFFF];
+        private int maxChild = 0; // optimization
         
         public CharTrieLevel0()
         {
-            this.key = (char)0;
+            super();
         }
         public CharTrieLevel0(char key)
         {
-            this.key = key;
+            super(key);
         }
         
+        @Override
         public boolean match(char c)
         {
             return key == c;
         }
         
+        @Override
         public boolean match(char[] chars, int offset)
         {
             if (offset == chars.length)
@@ -85,6 +159,7 @@ public abstract class CharTrie {
             return child.match(chars, offset + 1);
         }
         
+        @Override
         public void put(char[] chars, int offset)
         {
             if (offset >= chars.length) {
@@ -94,34 +169,67 @@ public abstract class CharTrie {
             char c = chars[offset];
             if (children[c] == null)
             {
-                if (offset == 0 || offset < 2 && c < 256)
-                {
-                      children[c] = new CharTrieLevel0(c);
-                }
-                else
-                {
-                    children[c] = new CharTrieLevel1(c);
-                }
+                children[c] = createNewChild(offset, c);
+                maxChild = Math.max(maxChild, (int)c);
             }
             children[c].put(chars, offset + 1);
+        }
+        
+        public CharTrie createNewChild(int offset, char c)
+        {
+            if (offset == 0 || offset < 2 && c < 256)
+            {
+                  return new CharTrieLevel0(c);
+            }
+            return new CharTrieLevel1(c);
+        }
+
+        @Override
+        public CharTrie getChild(char c)
+        {
+            return children[c];
+        }
+    }
+    
+    public static class OrderedCharTrieLevel0 extends CharTrieLevel0 {
+        
+        public OrderedCharTrieLevel0()
+        {
+            super();
+        }
+        
+        public OrderedCharTrieLevel0(char key)
+        {
+            super(key);
+        }
+        
+        @Override
+        public CharTrie createNewChild(int offset, char c)
+        {
+            if (offset == 0 || offset < 2 && c < 256)
+            {
+                  return new OrderedCharTrieLevel0(c);
+            }
+            return new OrderedCharTrieLevel1(c);
         }
     }
 
     public static class CharTrieLevel1 extends CharTrie {
 
-        public final char key;
         public List<CharTrie> children = new ArrayList<CharTrie>();
         
         public CharTrieLevel1(char key)
         {
-            this.key = key;
+            super(key);
         }
         
+        @Override
         public boolean match(char c)
         {
             return key == c;
         }
         
+        @Override
         public boolean match(char[] chars, int offset)
         {
             if (offset >= chars.length)
@@ -135,16 +243,19 @@ public abstract class CharTrie {
             return false;
         }
         
+        @Override
         public void put(char[] chars, int offset)
         {
-            if (offset >= chars.length) {
+            if (offset >= chars.length)
+            {
                 terminal = true;
                 return;
             }
             char c = chars[offset];
             for(CharTrie child : children)
             {
-                if (child.match(c)) {
+                if (child.match(c))
+                {
                     child.put(chars, offset + 1);
                     return;
                 }
@@ -153,6 +264,64 @@ public abstract class CharTrie {
             children.add(child);
             child.put(chars, offset+1);
         }
+        
+        @Override
+        public CharTrie getChild(char c)
+        {
+            for(CharTrie child : children)
+            {
+                if (child.match(c))
+                {
+                    return child;
+                }
+            }
+            return null;
+        }
     }
     
+    public static class OrderedCharTrieLevel1 extends CharTrieLevel1 {
+        
+        public OrderedCharTrieLevel1(char key)
+        {
+            super(key);
+        }
+        
+        public boolean match(char[] chars, int offset)
+        {
+            if (offset >= chars.length)
+                return terminal;
+            char c = chars[offset];
+            int index = Collections.binarySearch(children, new DummyCharTrie(c));
+            if (index < 0)
+                return false;
+            CharTrie child = children.get(index);
+            boolean ret = child.match(chars, offset+1);
+            return ret;
+        }
+        
+        public void put(char[] chars, int offset)
+        {
+            if (offset >= chars.length) {
+                terminal = true;
+                return;
+            }
+            char c = chars[offset];
+            CharTrie child = new CharTrieLevel1(c);
+            int index = Collections.binarySearch(children, new DummyCharTrie(c));
+            if (index >= 0)
+            {
+                children.set(index, child);
+            }
+            else
+            {
+                children.add(-index - 1, child);
+            }
+            child.put(chars, offset + 1);
+        }
+        @Override
+        public CharTrie getChild(char c)
+        {
+            return children.get(c);
+        }
+    }
 }
--- src/test/java/util/CharTrieTest.java
+++ src/test/java/util/CharTrieTest.java
@@ -1,9 +1,9 @@
 package util;
 
 
-import util.CharTrie;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import util.CharTrie.CharTrieLevel1;
 
 
 public class CharTrieTest {
@@ -23,4 +23,19 @@ public class CharTrieTest {
     }
 
 
+    @Test
+    public void testOrdered() {
+        CharTrie c = CharTrie.newOrderedTrie();
+        c.put("abac".toCharArray());
+        c.put("ababa".toCharArray());
+        c.put("aäð".toCharArray());
+        assertEquals(3, c.size());
+        assertTrue(c.match("abac".toCharArray()));
+        assertFalse(c.match("abcd".toCharArray()));
+        assertTrue(c.match("aäð".toCharArray()));
+        assertFalse(c.match("aäð-".toCharArray()));
+        assertFalse(c.match("ab".toCharArray()));
+        assertNotNull(c.getChild('a').getChild('b').getChild('a'));
+        assertEquals('c', ((CharTrieLevel1)(c.getChild('a').getChild('b').getChild('a'))).children.get(1).key);
+    }
 }
